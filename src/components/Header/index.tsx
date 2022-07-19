e9ce2bedefd7742dc3f6dd17613ae3f2532d6560
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-07-19 16:27:58 -0300

    Started styles from header and layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,17 @@
+import { HeaderContainer } from "./styles";
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href="">
+          timer
+        </a>
+        <a href="">
+          history
+        </a>
+      </nav>
+    </HeaderContainer>
+  )
+}
