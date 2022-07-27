61563fe42c0d289f86e2db659c627719ed065d41
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-07-27 11:42:23 -0300

    Stylized countdown container

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,34 @@
+import { Play } from "phosphor-react";
+import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+
 export function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="">Vou trabalhar em</label>
+          <input id="task"/>
+
+          <label htmlFor="">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <button type="submit">
+          <Play />
+          Começar
+        </button>
+      </form>
+
+    </HomeContainer>
   )
 }
