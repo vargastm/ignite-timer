ca5c6c4ae5ac43b2e3d2533efc6109ca2ef3715a
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-08-11 12:45:42 -0300

    Validated form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,27 @@
-import { Play } from "phosphor-react";
+import { Play } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
-import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import { 
+  CountdownContainer, 
+  FormContainer, 
+  HomeContainer, 
+  MinutesAmountInput, 
+  Separator, 
+  StartCountdownButton, 
+  TaskInput 
+} from "./styles";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5).max(60)
+})
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(),
+  })
   
   function handleCreateNewCycle(data: any) {
   }
