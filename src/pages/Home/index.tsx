8dfa7ffaf955c21da4fc49d829d183fccbc0353d
Tiago Martins Vargas <vargas.tiago@hotmail.com>
2022-08-19 15:11:28 -0300

    Changed title page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -73,6 +73,7 @@ export function Home() {
 
     setCycles((state) => [...cycles, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
 
     reset()
   }
@@ -86,6 +87,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
+  useEffect(() => {
+    if (activeCycle) {
+    document.title = `${minutes}:${seconds}`
+  }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task')
   const isSubmitDisabled = !task
