34a7dfa35a2133b870157aa9cf689fcecb48aa1d
Tiago Martins <vargas.tiago@hotmail.com>
2024-12-26 11:13:43 -0300

    feat: Add infos to story page

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -15,6 +15,7 @@ interface Cycle {
 }
 
 interface CyclesContextType {
+  cycles: Cycle[]
   activeCycle: Cycle | undefined
   activeCycleId: string | null
   amountSecondsPassed: number
@@ -68,8 +69,6 @@ export function CyclesContextProvider({
     setCycles((state) => [...cycles, newCycle])
     setActiveCycleId(id)
     setAmountSecondsPassed(0)
-
-    // reset()
   }
 
   function interruptCurrentCycle() {
@@ -89,6 +88,7 @@ export function CyclesContextProvider({
   return (
     <CyclesContext.Provider
       value={{
+        cycles,
         activeCycle,
         activeCycleId,
         markCurrentCycleAsFinished,
